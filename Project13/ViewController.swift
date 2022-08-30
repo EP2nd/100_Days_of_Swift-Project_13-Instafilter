5195bbdc43225b0650be313e543cb815300fccea
EPJrWorld <edwin.przezwiecki.jr@icloud.com>
2022-08-30 17:25:39 +0200

    Third challenge implementation

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -11,12 +11,25 @@ import CoreImage
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var imageView: UIImageView!
+    
     @IBOutlet var intensity: UISlider!
+    @IBOutlet var intensityLabel: UILabel!
+    
+    @IBOutlet var radius: UISlider!
+    @IBOutlet var radiusLabel: UILabel!
+    
+    @IBOutlet var scale: UISlider!
+    @IBOutlet var scaleLabel: UILabel!
+    
+    @IBOutlet var center: UISlider!
+    @IBOutlet var centerLabel: UILabel!
+    
     @IBOutlet var changeFilterButton: UIButton!
     
     var currentImage: UIImage!
     var context: CIContext!
     var currentFilter: CIFilter!
+    var filters = ["CIBumpDistortion", "CIGaussianBlur", "CIPixellate", "CISepiaTone", "CITwirlDistortion", "CIUnsharpMask", "CIVignette"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,13 +45,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBAction func changeFilter(_ sender: Any) {
         
         let alertController = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
-        alertController.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
-        alertController.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        
+        for filter in filters {
+            alertController.addAction(UIAlertAction(title: filter, style: .default, handler: setFilter))
+        }
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
         present(alertController, animated: true)
@@ -57,6 +67,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    @IBAction func radiusChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    @IBAction func scaleChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    @IBAction func centerChanged(_ sender: Any) {
         applyProcessing()
     }
     
@@ -81,20 +103,55 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func applyProcessing() {
+        
         //guard let image = currentFilter.outputImage else { return }
         //currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
         
         let inputKeys = currentFilter.inputKeys
         
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
-        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
+        DispatchQueue.main.async {
+            if inputKeys.contains(kCIInputIntensityKey) {
+                self.intensity.isEnabled = true
+                self.intensityLabel.isEnabled = true
+                self.currentFilter.setValue(self.intensity.value, forKey: kCIInputIntensityKey)
+            } else {
+                self.intensity.isEnabled = false
+                self.intensityLabel.isEnabled = false
+            }
+            
+            if inputKeys.contains(kCIInputRadiusKey) {
+                self.radius.isEnabled = true
+                self.radiusLabel.isEnabled = true
+                self.currentFilter.setValue(self.radius.value * 200, forKey: kCIInputRadiusKey)
+            } else {
+                self.radius.isEnabled = false
+                self.radiusLabel.isEnabled = false
+            }
+            
+            if inputKeys.contains(kCIInputScaleKey) {
+                self.scale.isEnabled = true
+                self.scaleLabel.isEnabled = true
+                self.currentFilter.setValue(self.scale.value * 10, forKey: kCIInputScaleKey)
+            } else {
+                self.scale.isEnabled = false
+                self.scaleLabel.isEnabled = false
+            }
+        
+        if inputKeys.contains(kCIInputCenterKey) {
+                self.center.isEnabled = true
+                self.centerLabel.isEnabled = true
+                self.currentFilter.setValue(CIVector(x: self.currentImage.size.width / 2, y: self.currentImage.size.height / 2), forKey: kCIInputCenterKey)
+            } else {
+                self.center.isEnabled = false
+                self.centerLabel.isEnabled = false
+            }
+        }
         
         /* if let cgimg = context.createCGImage(image, from: image.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             imageView.image = processedImage
         } */
+        
         if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             self.imageView.image = processedImage
@@ -102,7 +159,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func setFilter(action: UIAlertAction) {
-        // Making sure I have a valid image before continuing.
+        // Making sure there is a valid image before continuing.
         guard currentImage != nil else { return }
         
         // Safely read the alert action's title.
